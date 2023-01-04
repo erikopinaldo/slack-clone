a5966a90b864ba69e3399a48d3f5f5ac066c8bf4
Erik Opinaldo <erikopinaldo@gmail.com>
2023-01-04 17:30:41 -0500

    set up room display on the front end based on namespace/workspace connection

--- public/joinNs.js
+++ public/joinNs.js
@@ -1,3 +1,4 @@
+
 function joinNs(endpoint) {
     // close connection to namespace when no longer in use
     if (nsSocket) {
@@ -5,4 +6,31 @@ function joinNs(endpoint) {
         nsSocket.close()
     }
     nsSocket = io(`http://localhost:9000${endpoint}`)
+    nsSocket.on('nsRoomLoad', (nsRooms) => {
+        let roomList = document.querySelector('.room-list');
+        roomList.innerHTML = ""
+        nsRooms.forEach((room) => {
+            let glyph;
+            if (room.privateRoom) {
+                glyph = 'lock'
+            } else {
+                glyph = 'globe'
+            }
+            roomList.innerHTML += `<li class="room"><span class="glyphicon glyphicon-${glyph}"></span>${room.roomTitle}</li>`
+        })
+
+        // add click listener to each room
+        let roomNodes = document.getElementsByClassName('room');
+        Array.from(roomNodes).forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                console.log("Someone Clicked on ", e.target.innerText)
+                joinRoom(e.target.innerText)
+            })
+        })
+
+        // Add room automatically
+        const topRoom = document.querySelector('.room')
+        const topRoomName = topRoom.innerText;
+        joinRoom(topRoomName)
+    })
 }
