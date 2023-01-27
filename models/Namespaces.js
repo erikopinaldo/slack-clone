e19b6e6d833220855d0a8d317d81b5ac35efef91
Erik Opinaldo <erikopinaldo@gmail.com>
2023-01-26 22:33:14 -0500

    add Namespace db model

---
+++ models/Namespaces.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const NamespaceSchema = new mongoose.Schema({
+    name: {
+        type: mongoose.Schema.Types.String,
+        unique: true,
+        required: true,
+    },
+    endpoint:
+    {
+        type: mongoose.Schema.Types.String,
+        unique: true,
+        required: true,
+    },
+    img: {
+        type: mongoose.Schema.Types.String,
+        unique: true,
+    },
+})
+
+NamespaceSchema.pre('save', function (next) {
+    this.name = this.name.toLowerCase()
+
+    next()
+})
+
+module.exports = mongoose.model('namespaces', NamespaceSchema)
