827d97e51c2b5555149154da434db00ac1dfd478
Erik Opinaldo <erikopinaldo@gmail.com>
2023-01-29 13:27:45 -0500

    change username variable to user

--- public/joinNs.js
+++ public/joinNs.js
@@ -52,7 +52,7 @@ function buildHTML(msg) {
             <img src="${msg.avatar}" />
         </div>
         <div class="user-message">
-            <div class="user-name-time">${msg.username} <span>${convertedDate}</span></div>
+            <div class="user-name-time">${msg.user} <span>${convertedDate}</span></div>
             <div class="message-text">${msg.text}</div>
         </div>
     </li>
