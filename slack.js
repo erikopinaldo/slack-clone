5e791b1aa0238b702501c0175ad626e6b6d306f3
Erik Opinaldo <erikopinaldo@gmail.com>
2023-02-22 16:25:34 -0500

    rename channels routes

--- slack.js
+++ slack.js
@@ -21,7 +21,7 @@ const io = socketio(expressServer)
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
-app.use("/post", postRoutes);
+app.use("/channels", channelsRoutes);
 
 // note that io.on === io.of('/').on ;)
 io.on('connection', (socket) => {
