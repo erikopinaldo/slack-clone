ba3e01d411526c42384e43a63208516d1b821650
Erik Opinaldo <erikopinaldo@gmail.com>
2023-02-22 16:39:16 -0500

    replace channels refs with rooms refs

--- routes/main.js
+++ routes/main.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
-const channelsController = require("../controllers/channels");
+const roomsController = require("../controllers/rooms");
 const { ensureAuth } = require("../middleware/auth");
 
 //Main Routes 
 router.get("/", homeController.getIndex);
-router.get("/channels", ensureAuth, channelsController.getProfile);
+router.get("/rooms", ensureAuth, roomsController.getRooms);
 
 //Routes for user login/signup
 router.get("/login", authController.getLogin);
