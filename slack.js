03e51a82562ece99d50f93892e0ad8eb2039da77
Erik Opinaldo <erikopinaldo@gmail.com>
2023-02-22 20:05:27 -0500

    set up flash errors

--- slack.js
+++ slack.js
@@ -40,6 +40,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+//Use flash messages for errors, info, ect...
+app.use(flash());
+
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
 app.use("/rooms", roomsRoutes);
