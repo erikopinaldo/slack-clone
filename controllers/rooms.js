5169ffb9354d2af00d10de6f9ce47eb35c959f62
Erik Opinaldo <erikopinaldo@gmail.com>
2023-03-08 09:06:48 -0500

    add rooms controller

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -1,14 +1,14 @@
-const Rooms = require("../models/Rooms");
+const models = require("../models");
 
 module.exports = {
   getRooms: async (req, res) => { 
-    console.log(req.user)
     try {
       //Since we have a session each request (req) contains the logged-in users info: req.user
       //console.log(req.user) to see everything
       //Grabbing just the posts of the logged-in user
       //Sending post data from mongodb and user data to ejs template
-      res.render("rooms");
+      let messageHistory = await models.Messages.find({ room: req.params.name.toLowerCase() }).exec()
+      res.render("rooms", { messageHistory: messageHistory });
     } catch (err) {
       console.log(err);
     }
