2e79b652fe922e98568857fa74e04f4f6d4d60e6
Erik Opinaldo <erikopinaldo@gmail.com>
2023-03-09 09:42:21 -0500

    add current room name value to be passed to ejs

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -8,7 +8,7 @@ module.exports = {
       //Grabbing just the posts of the logged-in user
       //Sending post data from mongodb and user data to ejs template
       let messageHistory = await models.Messages.find({ room: req.params.name.toLowerCase() }).exec()
-      res.render("rooms", { messageHistory: messageHistory });
+      res.render("rooms", { messageHistory: messageHistory, currentRoom: req.params.name });
     } catch (err) {
       console.log(err);
     }
