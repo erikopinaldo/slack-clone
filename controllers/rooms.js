55c12690711970937214f23fa9b4911eb8aafd37
Erik Opinaldo <erikopinaldo@gmail.com>
2023-03-12 21:37:22 -0400

    add join room socket events to controller

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -7,8 +7,34 @@ module.exports = {
       //console.log(req.user) to see everything
       //Grabbing just the posts of the logged-in user
       //Sending post data from mongodb and user data to ejs template
-      let messageHistory = await models.Messages.find({ room: req.params.name.toLowerCase() }).exec()
+
+      let roomName = req.params.name.toLowerCase()
+      let namespace = req.io.of('/anime')
+
+      namespace.on('connection', (socket) => {
+        console.log('joining room ' + roomName)
+
+        console.log(socket.rooms)
+        const roomToLeave = [...socket.rooms][1]
+
+        // leave old room
+        socket.leave(roomToLeave)
+        // updateUsersInRoom(namespace, roomToLeave)
+
+        // join the socket to the new room
+        socket.join(roomName.toLowerCase())
+                    // updateUsersInRoom(namespace, roomToJoin)
+
+        console.log(socket.rooms)
+      });
+
+      let messageHistory = await models.Messages.find({ room: roomName }).exec()
+      console.log('message history')
+      console.log(req.params.name)
+      console.log(messageHistory)
+      
       res.render("rooms", { messageHistory: messageHistory, currentRoom: req.params.name });
+
     } catch (err) {
       console.log(err);
     }
