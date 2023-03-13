80b609c7a48131994d3c8638586e56f0e76d67e4
Erik Opinaldo <erikopinaldo@gmail.com>
2023-03-13 07:58:06 -0400

    remove message and room join logic, emit join room event instead

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -3,31 +3,8 @@ const models = require("../models");
 module.exports = {
   getRooms: async (req, res) => { 
     try {
-      //Since we have a session each request (req) contains the logged-in users info: req.user
-      //console.log(req.user) to see everything
-      //Grabbing just the posts of the logged-in user
-      //Sending post data from mongodb and user data to ejs template
-
       let roomName = req.params.name.toLowerCase()
-      let namespace = req.io.of('/anime')
-
-      namespace.on('connection', (socket) => {
-        console.log('joining room ' + roomName)
-
-        console.log(socket.rooms)
-        const roomToLeave = [...socket.rooms][1]
-
-        // leave old room
-        socket.leave(roomToLeave)
-        // updateUsersInRoom(namespace, roomToLeave)
-
-        // join the socket to the new room
-        socket.join(roomName.toLowerCase())
-                    // updateUsersInRoom(namespace, roomToJoin)
-
-        console.log(socket.rooms)
-      });
-
+      req.io.of('/anime').emit('joinRoom', roomName)
       let messageHistory = await models.Messages.find({ room: roomName }).exec()
       console.log('message history')
       console.log(req.params.name)
