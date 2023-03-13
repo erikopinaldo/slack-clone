94fabe134e07aeb7ac6f90fc55f7fdcad3572d0e
Erik Opinaldo <erikopinaldo@gmail.com>
2023-03-13 08:39:50 -0400

    log io instance

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -3,12 +3,11 @@ const models = require("../models");
 module.exports = {
   getRooms: async (req, res) => { 
     try {
+      console.log(req.io)
       let roomName = req.params.name.toLowerCase()
       req.io.of('/anime').emit('joinRoom', roomName)
       let messageHistory = await models.Messages.find({ room: roomName }).exec()
-      console.log('message history')
-      console.log(req.params.name)
-      console.log(messageHistory)
+      console.log('visiting: ' + req.params.name)
       
       res.render("rooms", { messageHistory: messageHistory, currentRoom: req.params.name });
 
